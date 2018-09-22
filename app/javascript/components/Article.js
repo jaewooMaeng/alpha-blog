3660cda6820ca17ef039b8a89edf19e17b103a35
Jaewoo Maeng <maeng9584@gmail.com>
2018-09-22 12:38:50 +0900

    timestamp

--- app/javascript/components/Article.js
+++ app/javascript/components/Article.js
@@ -1,5 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+import  Timestamp from "react-timestamp"
+
 class Article extends React.Component {
   render () {
     return (
@@ -10,8 +13,12 @@ class Article extends React.Component {
         <div className="article-body">
           {this.props.description}
           <div className="article-meta-details">
-            <small>Created by: {this.props.author}, {this.props.created_at} ago,
-                  last updated: {this.props.updated_at} ago</small>  
+            <small>
+              Created by: {this.props.author}, 
+              &nbsp;
+              <Timestamp time={this.props.created_at} precision={3} />,
+              last updated: <Timestamp time={this.props.updated_at} precision={3} />
+            </small>  
           </div>
         </div>
       </React.Fragment>
